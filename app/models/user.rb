99a8286b27c65883effa9db729a6445859ea2bd2
ry <zijoyrb@gamil.com>
2020-06-02 12:46:12 +0900

    set validations
    - 名前（全角）
    - ふりがな（全角）
    - ユーザー名（英数混合）
    - 郵便番号（ハイフンを含む）

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  has_one :address, dependent: :destroy
+  accepts_nested_attributes_for :address
+ 
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角"}         
+  validates :nickname, presence: true, format: { with: /\A[a-z0-9]+\z/i, message: "半角英数"}
+  validates :first_name_reading, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナ"}
+  validates :last_name_reading, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナ"}
+         
 end
