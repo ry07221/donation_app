349f50a2fc1e6a0759f3ce20d9033f069df49514
ry <zijoyrb@gamil.com>
2020-06-02 14:28:56 +0900

    create donation files
    「rails g model donation」を実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_one :address, dependent: :destroy
-  accepts_nested_attributes_for :address
- 
+  has_one :donation, dependent: :destroy
+
+  
   validates :name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください。"}         
   validates :name_reading, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい。"}
   validates :nickname, presence: true, format: { with: /\A[a-z0-9]+\z/i, message: "は半角英数で入力してください。"}
