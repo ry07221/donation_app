6c8fd3c26dfb2dd7d46effb46b322b134187dccf
ry <zijoyrb@gamil.com>
2020-06-02 16:08:25 +0900

    created basement
    土台となるファイル生成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(user_params)
+    @user = UserDonation.new(user_params)
 
     respond_to do |format|
       if @user.save
