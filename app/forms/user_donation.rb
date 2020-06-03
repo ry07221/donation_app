06aee83123f513eaf2fe933249e04c05df9f782e
ry <zijoyrb@gamil.com>
2020-06-03 17:29:25 +0900

    fixed
    ストパラ修正

--- app/forms/user_donation.rb
+++ app/forms/user_donation.rb
@@ -11,6 +11,7 @@ class UserDonation
     User.create(name: name, name_reading: name_reading, nickname: nickname)
     Address.create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building_name: building_name)
     Donation.create(price: price)
+    # binding.pry
   end
 
  end
